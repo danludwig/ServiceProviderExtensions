cebb6118e6103f773bea585356484dd3d22ccf6a
Dan Ludwig <Dan.Ludwig@ucosmic.org>
2012-07-17 03:18:26 -0400

    Assemble project.

---
+++ ServiceProviderExtensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ServiceProviderExtensions")]
+[assembly: AssemblyDescription("Generic GetService[T] and GetServices[T] extension methods for the System.IServiceProvider interface.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Dan Ludwig")]
+[assembly: AssemblyProduct("ServiceProviderExtensions")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ef486e1d-28db-488a-9392-90b94c2f93f5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.*")]
---
+++ ServiceProviderExtensions/ServiceProviderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class ServiceProviderExtensions
+{
+    public static TService GetService<TService>(this IServiceProvider serviceProvider)
+    {
+        return (TService)serviceProvider.GetService(typeof(TService));
+    }
+
+    public static IEnumerable<TService> GetServices<TService>(this IServiceProvider serviceProvider)
+    {
+        // make IEnumerable<TService>
+        var genericEnumerable = typeof(IEnumerable<>).MakeGenericType(typeof(TService));
+
+        // invoke IServiceProvider.GetService(typeof(IEnumerable<TService>))
+        var servicesObject = serviceProvider.GetService(genericEnumerable);
+
+        // cast object to IEnumerable<object>
+        var servicesEnumerable = (IEnumerable<object>)servicesObject;
+
+        // when null, return an empty IEnumerable<TService>
+        if (servicesEnumerable == null) return Enumerable.Empty<TService>();
+
+        // otherwise, cast IEnumerable<object> to IEnumerable<TService>
+        var strongEnumerable = servicesEnumerable.Cast<TService>();
+
+        return strongEnumerable;
+    }
+}
